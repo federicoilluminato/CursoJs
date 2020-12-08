8c6a2e40bed26766bc69ea86c7c7fe47444bc07b
Federico Illuminato <federicoilluminato@hotmail.com>
2020-12-08 19:25:26 -0300

    version 1

---
+++ script.js
@@ -0,0 +1,13 @@
+class Vuelo{
+    constructor(origen,destino,clase,precio,cantidad,){
+    this.origen=origen
+    this.destino=destino
+    this.clase=clase
+    this.precio=precio
+    this.cantidad=cantidad
+
+
+    }
+}
+
+let vuelo1= new Vuelo("Buenos Aires","Madrid","Economica",26000,2);
