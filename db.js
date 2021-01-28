5a865e9a6ff19cabaa1759f60988fff6002a6a99
Federico Illuminato <federicoilluminato@hotmail.com>
2021-01-27 21:03:18 -0300

    adelanto de proyecto final

---
+++ db.js
@@ -0,0 +1,32 @@
+const vuelos = [
+    {
+        origen: "buenos aires",
+        destino: "madrid",
+        fecha: "15/01/2021",
+        precio: ["economica:","premium economy:","ejecutiva:","primera clase:"]
+    },
+    {
+        origen: "buenos aires",
+        destino: "madrid",
+        fecha: "20/01/2021",
+        precio: ["economica:","premium economy:","ejecutiva:","primera clase:"]
+    },
+    {
+        origen: "madrid",
+        destino: "rio de janeiro",
+        fecha: "19/01/2021",
+        precio: ["economica:","premium economy:","ejecutiva:","primera clase:"]
+    },
+    {
+        origen: "buenos aires",
+        destino: "madrid",
+        fecha: "21/01/2021",
+        precio: ["economica:","premium economy:","ejecutiva:","primera clase:"]
+    },
+
+
+
+
+
+
+]
--- script.js
+++ script.js
@@ -1,103 +1,131 @@
+//Variables datos ingresados
+const datoOrigen = document.getElementById("origen").value.toLowerCase();
+const datoDestino = document.getElementById("destino").value.toLowerCase();
+const datoIda = document.getElementById("ida").value.toLowerCase().toString();
+const datoVuelta = document.getElementById("vuelta").value.toLowerCase();
 
-const vuelos = [
-    {
-        "origen": "buenos aires",
-        "destino": "madrid",
-        "fecha": "15/01/2021",
-        "precio": ["economica:","premium economy:","ejecutiva:","primera clase:"]
-    },
-    {
-        "origen": "buenos aires",
-        "destino": "madrid",
-        "fecha": "20/01/2021",
-        "precio": ["economica:","premium economy:","ejecutiva:","primera clase:"]
-    },
-    {
-        "origen": "madrid",
-        "destino": "rio de janeiro",
-        "fecha": "19/01/2021",
-        "precio": ["economica:","premium economy:","ejecutiva:","primera clase:"]
-    },
-    {
-        "origen": "buenos aires",
-        "destino": "madrid",
-        "fecha": "21/01/2021",
-        "precio": ["economica:","premium economy:","ejecutiva:","primera clase:"]
-    },
+//Variables datos ingresados convertidos
+const datoIdaConvertido = datoIda.substring(8,10) + '/' + datoIda.substring(5,7) + '/' + datoIda.substring(0,4);
+const datoVueltaConvertido = datoVuelta.substring(8,10) + '/' + datoVuelta.substring(5,7) + '/' + datoVuelta.substring(0,4);
+
+var search = document.getElementById('search');
+const resultado = document.querySelector('#resultado');
+
+//Objeto de la busqueda
+const datosBusqueda = {
+    origen: '',
+    destino: '',
+    ida: '',
+    vuelta: '',
+    // : '',
+    // : '',
+}
+
+//Events
+search.addEventListener('click', function() {
+    filtrarVuelos();
+    // mostrarVuelos();
+    //  console.log(datosBusqueda);
+    // filtrarVuelos(datosBusqueda); 
+})
+
+origen.addEventListener('change', e => {
+    datosBusqueda.origen = e.target.value
+    // console.log(datosBusqueda.origen)
+
+})
+destino.addEventListener('change', e => {
+    datosBusqueda.destino = e.target.value
+    // console.log(datosBusqueda.destino)
+
+})
+ida.addEventListener('change', e => {
+    datosBusqueda.ida = e.target.value
+    // console.log(datosBusqueda.ida)
+
+})
+vuelta.addEventListener('change', e => {
+    datosBusqueda.vuelta = e.target.value
+    // console.log(datosBusqueda.vuelta)
+
+})
 
 
+//Funciones
 
+function mostrarVuelos () {
 
+    limpiarHTML();
+    vuelos.forEach(vuelo => {
+        const {origen, destino, fecha, precio} = vuelo;
+        const vueloHTML = document.createElement('p');
+        
 
+        vueloHTML.textContent = `
+        ${origen} ${destino} ${fecha} ${precio}
+        `;
 
-]
+        resultado.appendChild(vueloHTML);
+    })
+}
 
-
-// funcion del btn buscar que filtra los vuelos
-function buscar (){
-    const datoOrigen = (document.getElementById("origen").value.toLowerCase());
-    const datoDestino = (document.getElementById("destino").value.toLowerCase());
-    const datoIda = (document.getElementById("ida").value.toLowerCase().toString());
-    const datoIdaConvertido = datoIda.substring(8,10) + '/' + datoIda.substring(5,7) + '/' + datoIda.substring(0,4);
-    const datoVuelta = (document.getElementById("vuelta").value.toLowerCase());
-    const datoVueltaConvertido = datoVuelta.substring(8,10) + '/' + datoVuelta.substring(5,7) + '/' + datoVuelta.substring(0,4);
-
-    for (vueloElegido of vuelos) {
-         if ((datoOrigen == vueloElegido.origen) && (datoDestino == vueloElegido.destino)) {
-            $('#contenedorVuelos').append(`<div class="col-9">Origen: ${vueloElegido.origen}, destino: ${vueloElegido.destino}, fecha: ${vueloElegido.fecha}</div>`); 
-            //console.log(`Origen: ${datoOrigen}, destino: ${datoDestino}, fecha: ${vueloElegido.fecha}`);
-        } else if (datoOrigen == vueloElegido.origen) {
-            $('#contenedorVuelos').append(`<div class="col-9">Origen: ${vueloElegido.origen}, destino: ${vueloElegido.destino}, fecha: ${vueloElegido.fecha}</div>`); 
-            //console.log(`Origen: ${datoOrigen}, destino: ${vueloElegido.destino}, fecha: ${vueloElegido.fecha}`);
-        } else if (datoDestino == vueloElegido.destino) {
-            $('#contenedorVuelos').append(`<div class="col-9">Origen: ${vueloElegido.origen}, destino: ${vueloElegido.destino}, fecha: ${vueloElegido.fecha}</div>`); 
-            //console.log(`Origen: ${vueloElegido.origen}, destino: ${datoDestino}, fecha: ${vueloElegido.fecha}`);
-        } else if (datoIdaConvertido == vueloElegido.fecha) {
-            $('#contenedorVuelos').append(`<div class="col-9">Origen: ${vueloElegido.origen}, destino: ${vueloElegido.destino}, fecha: ${vueloElegido.fecha}</div>`); 
-            //console.log(`Origen: ${vueloElegido.origen}, destino: ${vueloElegido.destino}, fecha: ${datoIda}`);
-        } else if (datoVueltaConvertido == vueloElegido.fecha) {
-            $('#contenedorVuelos').append(`<div class="col-9">Origen: ${vueloElegido.origen}, destino: ${vueloElegido.destino}, fecha: ${vueloElegido.fecha}</div>`);   
-            //console.log(`Origen: ${vueloElegido.origen}, destino: ${vueloElegido.destino}, fecha: ${datoVuelta}`);
-        } 
+//Limpiar HTML
+function limpiarHTML () {  
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
     }
 }
 
 
-// Funcion radio-btn ida y vuelta o solo ida
 
-function radio(x){
-    if(x==0){
-    document.getElementById("vuelta").style.display="inline";}
-    else{
-        document.getElementById('vuelta').style.display="none";
-    }return;
+//Funcion que filtra en base a la busqueda
+
+function filtrarVuelos() {
+    const resultado = vuelos.filter( filtrarOrigen ).filter( filtrarDestino ).filter( filtrarIda ).filter( filtrarVuelta);
+
+    console.log(resultado);
+    
+    mostrarVuelos(resultado);
+
+    if( resultado.length ){
+        mostrarVuelos(resultado);
+    } else {
+        console.log('no hay resultado');
+    }
 }
 
 
-// Funcion de sugerencias
 
+function filtrarOrigen(vuelo){
+    const { origen } = datosBusqueda;
+    if( origen ){
+        return vuelo.origen === origen;
+    }
+    return vuelo;
+}
 
+function filtrarDestino(vuelo){
+    const { destino } = datosBusqueda;
+    if( destino ){
+        return vuelo.destino === destino;
+    }
+    return vuelo;
+}
 
-//Funcion Buscar
-var search = document.getElementById('search');
-search.addEventListener('click', function() {
-    buscar();
-})
+function filtrarIda (vuelo){
+    const { ida } = datosBusqueda;
+    if ( ida ){
+        return vuelo.fecha === ida;
+    }
+    return vuelo;
+}
 
-
-//function vuelosPrint() {
-//    return `
-//    <div class="section_vuelos container-fluid">
-
-//     <div class="container_vuelos container-sm">
-//     <div class="row" id="contenedorVuelos">
-//             <div class="col-9">Origen: ${datoOrigen}, destino: ${datoDestino}, fecha: ${vueloElegido.fecha}</div>
-//             <div class="col-9">Origen: ${vueloElegido.origen}, destino: ${datoDestino}, fecha: ${vueloElegido.fecha}</div>
-//             <div class="col-3">1</div>
-//         </div>
-//   </div>
-// </div>
-//     `
-// }
+function filtrarVuelta (vuelo){
+    const { vuelta } = datosBusqueda;
+    if ( vuelta ){
+        return vuelo.fecha === vuelta;
+    }
+    return vuelo;
+}
 
 
